293d5376dc8cdb11c697eb2225af3abddfb7d9f2
JGVSaramago <joao.g.198@gmail.com>
2017-10-16 19:12:59 +0100

    Correct adiciona_saldo.sh

--- adiciona_saldo.sh
+++ adiciona_saldo.sh
@@ -6,7 +6,7 @@ read addtobalance
 if [ $( cat utilizadores.txt | grep ^$nickname[\;] | wc -l ) -gt 0 ]; then
 if [[ $addtobalance =~ ^[-+]?[0-9]+$ ]]; then
         i=$(cat utilizadores.txt | grep ^$nickname[\;] | awk -F ";" '{print $7}' )
-        cat utilizadores.txt | grep ^$nickname[\;] | sed -i '' -e 's/;'$i'$/;'$(( $i + $addtobalance ))'/g' utilizadores.txt
+        cat utilizadores.txt | grep ^$nickname[\;] | sed -i -e 's/;'$i'$/;'$(( $i + $addtobalance ))'/g' utilizadores.txt
     else
         echo "The balance to add isn't a integer"
     fi
