086bdb8a9eaec16584f9ce5ab5982ea127c2b0bf
JGVSaramago <joao.g.198@gmail.com>
2017-11-09 18:59:27 +0000

    Added compatibility with CLion;

--- menu.sh
+++ menu.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 crontab /home/a77981/SOproject/cron.txt #Sempre que alguém abre o menu, o ficheiro cron.txt é adicionado ao crontab
-BOLD='tput bold'
-RED='tput setaf 1'
-RESET='tput sgr0'
+
 echo ""
-echo "$($BOLD)$($RED)### iCoolIUL Menu ###$($RESET)"
+echo "### iCoolIUL Menu ###"
 echo ""
-echo "$($BOLD)1.$($RESET) Create user"
-echo "$($BOLD)2.$($RESET) Change balance"
-echo "$($BOLD)3.$($RESET) Create vehicle"
-echo "$($BOLD)4.$($RESET) Show users"
-echo "$($BOLD)5.$($RESET) Show stats"
-echo "$($BOLD)6.$($RESET) Backup"
-echo "$($BOLD)0.$($RESET) Exit"
+echo "1. Create user"
+echo "2. Change balance"
+echo "3. Create vehicle"
+echo "4. Show users"
+echo "5. Show stats"
+echo "6. Backup"
+echo "0. Exit"
 echo ""
 echo "Choose one option:"
 echo ""
