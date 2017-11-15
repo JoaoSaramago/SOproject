7a00be23496e6e41e31a92cadc19057c5cfbaf76
Andre Silva <a77981@iscte.pt>
2017-11-15 15:14:09 +0000

    Update servidor.c

--- servidor.c
+++ servidor.c
@@ -2,64 +2,69 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
+#include <string.h>
 
 int pid;
 
+void printToScreen(char* string){
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	
+	printf("%d-%d-%d  %d:%d:%d - %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, string);
+	FILE *file = fopen("servidor.log", "ab");
+	fprintf(file, "%d-%d-%d  %d:%d:%d - %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, string);
+	fclose(file);
+}
+
 void closeHandlerFather(){
-    remove("servidor.pid");
-    kill(pid, SIGINT);
-    printf("Father closing.\n");
-    FILE *file = fopen("servidor.log", "a");
-    fprintf(file, "Father closing.\n");
-    fclose(file);
-    exit(0);
+	remove("servidor.pid");
+	kill(pid, SIGINT);
+	printToScreen("Father closing.");
+	exit(0);
 }
 
 void closeHandlerChild(){
-    printf("Fiscal is closing.\n");
-    FILE *file = fopen("servidor.log", "a");
-    fprintf(file, "Fiscal is closing.\n");
-    fclose(file);
-    exit(0);
+    printToScreen("Fiscal is closing.");
+	exit(0);
 }
 
 void alarmHandlerChild(){
-    printf("Fiscal is verifying the data.\n");
-    FILE *file = fopen("servidor.log", "a");
-    fprintf(file, "Fiscal is verifying the data.\n");
-    fclose(file);
+	alarm(60);
+	printToScreen("Fiscal is verifying the data.");
 }
 
 int main(){
+	char temp[20];
+	sprintf(temp, "PID: %d", getpid());
+	printToScreen(temp);
+	
+	FILE *pidFile = fopen("servidor.pid", "w");
+	FILE *logFile = fopen("servidor.log", "a");
 
-    printf("PID: %d\n", getpid() );
-
-    FILE *pidFile = fopen("../servidor.pid", "w");
-    FILE *logFile = fopen("../servidor.log", "a");
-
-    if(pidFile == NULL){
-        printf("Error opening file!\n");
-        fprintf(logFile, "Error opening file!\n");
-        exit(1);
-    }
-
-    fprintf(pidFile,"PID: %d\n", getpid() );
-    fprintf(logFile, "PID: %d\n", getpid() );
-
-    fclose(pidFile);
+	if(pidFile == NULL){
+		printf("Error opening file!\n");
+		fprintf(logFile, "Error opening file!\n");
+		exit(1);
+	}
+	
+	fprintf(pidFile,"PID: %d\n", getpid() );
+	fprintf(logFile, "PID: %d\n", getpid() );
+	
+	fclose(pidFile);
     fclose(logFile);
-
-    pid = fork();
-
-    if(pid==0){ //Child (Fiscal)
-        signal(SIGINT, closeHandlerChild);
-        signal(SIGALRM, alarmHandlerChild);
-        alarm(60);
-        while(1) sleep(1);
-    }
-
-    if(pid>0){ //Father
-        signal(SIGINT, closeHandlerFather);
-        while(1) sleep(1);
-    }
+	
+	pid = fork();
+	
+	if(pid==0){ //Child (Fiscal)
+		signal(SIGINT, closeHandlerChild);
+		signal(SIGALRM, alarmHandlerChild);
+		alarm(60);
+		while(1) sleep(1);
+	}
+	
+	if(pid>0){ //Father
+		signal(SIGINT, closeHandlerFather);
+		while(1) sleep(1);
+	}
 }
